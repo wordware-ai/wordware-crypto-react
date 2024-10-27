f7bb65cb5ee07a7f9b98bf0a4377c3daaf63238f
William Phan <willdphan@gmail.com>
2024-10-27 06:10:30 -0700

    progress

--- app/components/Progress.tsx
+++ app/components/Progress.tsx
@@ -47,27 +47,24 @@ const ProgressItem: React.FC<
   action,
 }) => {
   const getIcon = () => {
+    const iconClass = "fill-white w-7 h-7 p-1"; // Square SVG with white fill
     switch (type) {
       case "NEXT":
         return (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="white"
-            width="35"
-            height="35"
+            className={iconClass}
           >
-            <path d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM12 20C16.42 20 20 16.42 20 12C20 7.58 16.42 4 12 4C7.58 4 4 7.58 4 12C4 16.42 7.58 20 12 20ZM13 12H16L12 16L8 12H11V8H13V12Z"></path>
+            <path d="M13.0001 16.1716L18.3641 10.8076L19.7783 12.2218L12.0001 20L4.22192 12.2218L5.63614 10.8076L11.0001 16.1716V4H13.0001V16.1716Z"></path>
           </svg>
         );
       case "ANSWER":
         return (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="white"
-            width="35"
-            height="35"
             viewBox="0 0 24 24"
+            className={iconClass}
           >
             <path d="M11.602 13.7599L13.014 15.1719L21.4795 6.7063L22.8938 8.12051L13.014 18.0003L6.65 11.6363L8.06421 10.2221L10.189 12.3469L11.6025 13.7594L11.602 13.7599ZM11.6037 10.9322L16.5563 5.97949L17.9666 7.38977L13.014 12.3424L11.6037 10.9322ZM8.77698 16.5873L7.36396 18.0003L1 11.6363L2.41421 10.2221L3.82723 11.6352L3.82604 11.6363L8.77698 16.5873Z"></path>
           </svg>
@@ -77,9 +74,7 @@ const ProgressItem: React.FC<
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="white"
-            width="35"
-            height="35"
+            className={iconClass}
           >
             <path d="M24 12L18.3431 17.6569L16.9289 16.2426L21.1716 12L16.9289 7.75736L18.3431 6.34315L24 12ZM2.82843 12L7.07107 16.2426L5.65685 17.6569L0 12L5.65685 6.34315L7.07107 7.75736L2.82843 12ZM9.78845 21H7.66009L14.2116 3H16.3399L9.78845 21Z"></path>
           </svg>
@@ -89,9 +84,7 @@ const ProgressItem: React.FC<
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="white"
-            width="35"
-            height="35"
+            className={iconClass}
           >
             <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM13 12H16L12 16L8 12H11V8H13V12Z" />
           </svg>
@@ -106,24 +99,28 @@ const ProgressItem: React.FC<
       onMouseLeave={() => onHover(-1)}
     >
       <motion.div
-        className={`px-6 py-3 text-md flex flex-col items-center bg-[#548E28] text-white rounded-lg transition-shadow duration-200 ease-in-out w-[280px]`}
+        className={`px-3 py-3 text-md flex flex-col items-center bg-white text-black rounded-lg transition-shadow duration-200 ease-in-out max-w-[280px] border border-gray-200`}
         whileHover={{
-          boxShadow: "0 0 4px 4px rgba(84, 142, 40, 0.3)",
+          boxShadow: "0 0 6px 2px rgba(84, 142, 40, 0.25)", // Changed from 0.3 to 0.15
         }}
       >
         <div className="flex items-center justify-center w-full">
           <div className="flex items-center">
-            <span className="mr-5">{getIcon()}</span>
-            <div className="flex flex-col">
-              <div className="uppercase">{label}</div>
-              <div className="text-sm">
+            <span className="mr-3 flex-shrink-0 bg-[#548E28] p-1 rounded-md">
+              {getIcon()}
+            </span>
+            <div className="flex flex-col flex-grow min-w-0">
+              <div className="uppercase font-medium truncate">{label}</div>
+              <div className="text-sm truncate">
                 {isSummarized ? summarizedDescription : description}
               </div>
             </div>
           </div>
         </div>
         {action && (
-          <div className="text-xs mt-1 text-center">Action: {action}</div>
+          <div className="text-xs mt-1 text-center w-full truncate">
+            Action: {action}
+          </div>
         )}
       </motion.div>
       <AnimatePresence>
@@ -133,9 +130,9 @@ const ProgressItem: React.FC<
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
             transition={{ duration: 0.2, ease: "easeInOut" }}
-            className="absolute top-full mt-2 p-2 bg-white border border-gray-300 rounded-lg shadow-lg z-20 w-[300px] overflow-hidden"
+            className="absolute top-full mt-2 p-2 bg-white border border-gray-300 rounded-lg shadow-lg z-20 w-[280px] overflow-hidden"
           >
-            <p className="text-sm text-black text-center ">{description}</p>
+            <p className="text-sm text-black text-center">{description}</p>
           </motion.div>
         )}
       </AnimatePresence>
@@ -253,7 +250,7 @@ const Progress: React.FC<ProgressProps> = ({ generations = [] }) => {
 
   return (
     <div className="w-full h-full flex flex-col justify-center  items-center bg-[#FDFAF5] relative overflow-hidden dotted-background">
-      <div className="absolute left-1/2 top-0 bottom-0 w-px bg-[#AFAFAF] transform -translate-x-1/2">
+      <div className="absolute left-1/2 top-0 bottom-0 w-px bg-[#969696] transform -translate-x-1/2">
         <div
           className="absolute top-0 left-0 w-0.5 bg-[#AFAFAF]  transition-all duration-300 ease-in-out"
           style={{
